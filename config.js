97253ad492c4b329ac557a697396b8c39cb0581d
bq <40911640+thebaqnk@users.noreply.github.com>
2026-03-15 13:52:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const CONFIG = {
-    // ระบบจะดึงค่าจากส่วนนี้ไปใช้งาน
-    API_ENDPOINT: "GAS_API", // ระบุ URL ของ Web App
-    LIFF_ID: "LIFF_ID",      // ระบุ LIFF ID จาก LINE Developers
-    VERSION: "1.0.4"
+  LIFF_ID: "process.env.LIFF_ID",
+  API_ENDPOINT: "process.env.GAS_API"
 };
