5dae9f323ae6f497e943da8b43836eb443a4fb14
bq <40911640+thebaqnk@users.noreply.github.com>
2026-03-15 11:46:45 +0700

    อัพโหลดส่วนของ Private_URL

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    // ระบบจะดึงค่าจากส่วนนี้ไปใช้งาน
+    API_ENDPOINT: "GAS_API", // ระบุ URL ของ Web App
+    LIFF_ID: "LIFF_ID",      // ระบุ LIFF ID จาก LINE Developers
+    VERSION: "1.0.4"
+};
